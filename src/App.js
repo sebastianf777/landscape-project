7f42ff735ba11f5adbedef64998f7efe6c2f0d71
Sebastian Fontana <sebastianf777@outlook.com>
2021-12-06 10:09:43 -0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import Navbar from './components/Navbar/Navbar';
+import './App.css';
+import Home from './components/Pages/Home';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Services from './components/Pages/Services';
+import Products from './components/Pages/Products';
+import SignUp from './components/Pages/SignUp';
+
+function App() {
+  return (
+    <>
+      <Router>
+        <Navbar />
+      
+          <Switch>
+            <Route path='/' exact component={Home} />
+            <Route path='/services' component={Services} />
+            <Route path='/products' component={Products} />
+            <Route path='/sign-up' component={SignUp} />
+          </Switch>
+     
+      </Router>
+    </>
+  );
+}
+
+export default App;
